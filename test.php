a7c2edfb85737a150b782c113be0b50fd0112e6a
himanshudwivedi <h.dwivedi@accenture.com>
2019-05-02 11:38:26 +0530

    new test file

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php 
+$x = "Sandeep"; 
+y = "Rout"; 
+echo $x; 
+echo $y; 
+function add($a, $b) 
+{ 
+    $sum = $a + $b; 
+    echo "sum = " . $sum; 
+} 
+$a = 0; 
+$b = 20; 
+add($x, $y); 
+  
+diff($x, $y); 
+include ("a.php"); 
+echo $c;
+echo 'hi'
+?> 
+?> 
